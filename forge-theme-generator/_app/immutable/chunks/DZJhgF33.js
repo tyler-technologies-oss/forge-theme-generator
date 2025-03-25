67321f8afceb173301d66b6ba8349dff741a3b62
nickonometry <nickonometry@gmail.com>
2025-03-25 11:41:36 -0400

    chore: quick fix to adjust folder site is served from

---
+++ forge-theme-generator/_app/immutable/chunks/DZJhgF33.js
@@ -0,0 +1,1 @@
+import{z as t,A as o,B as c,u}from"./CQ4WrLjx.js";function l(n){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function r(n){t===null&&l(),o&&t.l!==null?a(t).m.push(n):c(()=>{const e=u(n);if(typeof e=="function")return e})}function a(n){var e=n.l;return e.u??(e.u={a:[],b:[],m:[]})}export{r as o};
--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,11 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({ strict: false, fallback: 'index.html', pages: 'docs' }),
-		prerender: {
-			handleHttpError: 'warn'
-		}
-	}
+  kit: {
+    adapter: adapter({ strict: false, fallback: 'index.html', pages: 'forge-theme-generator' }),
+    prerender: {
+      handleHttpError: 'warn'
+    }
+  }
 };
 export default config;
